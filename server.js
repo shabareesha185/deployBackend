fc926e8f095a411de5db13f68e8013cf77897646
Shabareesha <shabareesha41699@gmail.com>
2024-08-08 02:44:27 +0530

    added about me

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get('/about',(req,res) => {
+  res.send('<h1>Im a student');
+}
+)
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
   

d83e38e827b99f1f9f20cd2a9b5c6ba87ed79b7e
Shabareesha <shabareesha41699@gmail.com>
2024-08-08 05:52:17 +0530

    added Jokes

--- server.js
+++ server.js
@@ -1,18 +1,58 @@
-require('dotenv').config();
-const express = require("express");
+import express from "express";
+
 const app = express();
-const port = 4000;
+
+// app.use(express.static('dist'));
+
+const jokes = [
+  {
+    question: "Why don't scientists trust atoms?",
+    answer: "Because they make up everything!",
+  },
+  {
+    question: "Why did the scarecrow win an award?",
+    answer: "Because he was outstanding in his field!",
+  },
+  {
+    question: "Why don't skeletons fight each other?",
+    answer: "They don't have the guts.",
+  },
+  {
+    question: "What do you get when you cross a snowman and a vampire?",
+    answer: "Frostbite.",
+  },
+  {
+    question: "Why did the bicycle fall over?",
+    answer: "Because it was two-tired!",
+  },
+  {
+    question: "How does a penguin build its house?",
+    answer: "Igloos it together.",
+  },
+  {
+    question: "Why was the math book sad?",
+    answer: "Because it had too many problems.",
+  },
+  { question: "What do you call fake spaghetti?", answer: "An impasta." },
+  {
+    question: "Why did the golfer bring an extra pair of pants?",
+    answer: "In case he got a hole in one.",
+  },
+  {
+    question: "What do you call cheese that isn't yours?",
+    answer: "Nacho cheese.",
+  },
+];
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Server Started");
 });
 
-app.get('/about',(req,res) => {
-  res.send('<h1>Im a student');
-}
-)
+app.get("/api/jokes", (req, res) => {
+  res.send(jokes);
+});
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`);
-  
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server Started at port: http://localhost:${port}`);
 });
